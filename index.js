d14ad5a47861c0e1b6ccb1828f9d63e94b244a76
Petros Tsavdaridis <peteropon@gmail.com>
2021-04-13 21:03:44 +0200

    Added resolver and some mock data.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
-    members: [Member]
+    members(name: String, department: String, role: String): [Member]
   }
   type Member {
     name: String
@@ -11,8 +11,42 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    members: () => members,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Graphql running at ${url}`);
 });
+
+const members = [
+  {
+    name: "jon",
+    department: "ddd",
+    role: "leader",
+  },
+  {
+    name: "kim",
+    department: "mmm",
+    role: "dev",
+  },
+  {
+    name: "pam",
+    department: "ddd",
+    role: "dev",
+  },
+  {
+    name: "fos",
+    department: "mmm",
+    role: "dev",
+  },
+  {
+    name: "io",
+    department: "ddd",
+    role: "dev",
+  },
+];
