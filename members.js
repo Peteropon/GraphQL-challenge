28b0376a19ff6c986c6846d3fc45abd18dffc0cd
Petros Tsavdaridis <peteropon@gmail.com>
2021-04-14 14:57:34 +0200

    Moved members to own file.

---
+++ members.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    id: "1",
+    name: "jon",
+    department: "ddd",
+    role: "leader",
+  },
+  {
+    id: "2",
+    name: "kim",
+    department: "mmm",
+    role: "dev",
+  },
+  {
+    id: "3",
+    name: "pam",
+    department: "ddd",
+    role: "dev",
+  },
+  {
+    id: "4",
+    name: "fos",
+    department: "mmm",
+    role: "dev",
+  },
+  {
+    id: "5",
+    name: "io",
+    department: "ddd",
+    role: "dev",
+  },
+];
